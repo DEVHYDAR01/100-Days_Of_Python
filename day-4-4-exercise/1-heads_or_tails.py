6df5bb06175eaefbd3904d7cdc513e99bdf87d36
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-02-09 00:23:05 +0100

    This commit will push head or tail with random

--- day-4-4-exercise/1-heads_or_tails.py
+++ day-4-4-exercise/1-heads_or_tails.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python3
-
+import random
+# Don't change the code below
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+# Don't change the code above
+# Write your code below this line
+#Hint: Remember to import the random module first. 🎲
+seed_toss = random.randint(1, 1000)
+if seed_toss == 833:
+    print("Heads")
+elif seed_toss == 677:
+    print("Tails")
+else:
+    print("Not a random")
---
+++ day-4-4-exercise/unpredict.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import random
+
+seed = random.seed(134)
+
+ranint = random.randint(1, 2000)
+print(ranint)
