ecda39b0eca2d4338abe20b51adb2cca05ecfec8
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-03-27 18:18:04 +0100

    This commit will push task 2

--- day-5-5-exercise/2-highest_number.py
+++ day-5-5-exercise/2-highest_number.py
@@ -4,7 +4,11 @@
 student_scores = input("Input a list of student scores ").split()
 for n in range(0, len(student_scores)):
   student_scores[n] = int(student_scores[n])
-print(student_scores)
 # 🚨 Don't change the code above 👆
 
 #Write your code below this row 👇
+max_value = student_scores[0]
+for i in student_scores:
+    if i > max_value:
+        max_value = i
+print(f"The highest score in the class is: {max_value}")
