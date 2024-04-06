23ff9ce849852422d0b3298b48aa442b03ee3659
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-04-06 18:44:29 +0100

    This commit will push escaping the maze

---
+++ day-6-6-exercise/Final_Project_Escaping_The_Maze.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def turn_around():
+    turn_left()
+    turn_left()
+    
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+        
+    
+    
+    
+
+    
+
+
+    
+
+
+
+
+
+
+
