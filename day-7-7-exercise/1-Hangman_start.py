30a900d1f9cb39fb9441a38b93e90806a2dbfcae
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-04-09 15:37:22 +0100

    This commit will push some changes

--- day-7-7-exercise/1-Hangman_start.py
+++ day-7-7-exercise/1-Hangman_start.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+
+#Step 1 
+import random
+word_list = ["aardvark", "baboon", "camel"]
+
+#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+chosen_word = random.choice(word_list)
+print(chosen_word)
+#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+convert_to_lower = input("Guess a letter: ")
+guess = convert_to_lower.lower()
+print(guess)
+#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+for letter in chosen_word:
+    if letter == guess:
+        print("right")
+    else:
+        print("wrong")
