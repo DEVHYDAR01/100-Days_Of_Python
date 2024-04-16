a964a560197b87e51e902238150645738aa9a8de
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-04-16 23:34:35 +0100

    This commit will push 1

--- day-6-6-exercise/1-challenge_i_and_ii.py
+++ day-6-6-exercise/1-challenge_i_and_ii.py
@@ -4,14 +4,6 @@ def turn_right():
     turn_left()
     turn_left()
     turn_left()
-move()
-move()
-move()
-turn_left()
-move()
-move()
-move()
-turn_right()
 
 #reeborgs  instruction challenge make a square
 def turn_right():
