c36f4da164d85d8c1a440f66fbaac9a744da7b0e
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-04-22 13:03:41 +0100

    This commit will push hangman

--- day-7-7-exercise/4-Hangman_punishment_sticks.py
+++ day-7-7-exercise/4-Hangman_punishment_sticks.py
@@ -59,7 +59,6 @@ stages = ['''
       |
 =========
 ''']
-
 end_of_game = False
 word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
@@ -67,6 +66,7 @@ word_length = len(chosen_word)
 
 #TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
 #Set 'lives' to equal 6.
+lives = 6
 
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
@@ -89,6 +89,12 @@ while not end_of_game:
     #TODO-2: - If guess is not a letter in the chosen_word,
     #Then reduce 'lives' by 1. 
     #If lives goes down to 0 then the game should stop and it should print "You lose."
+    if not guess in chosen_word:
+      lives = lives - 1
+      print(lives)
+    if lives == 0:
+      print("You lose!")
+      break
 
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
@@ -99,3 +105,6 @@ while not end_of_game:
         print("You win.")
 
     #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+    for pos in range(len(stages)):
+       if lives == pos:
+          print(stages[pos])
---
+++ day-7-7-exercise/list.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+my_list = [1, 2, 3]
+my_list.reverse()
+print(my_list)
