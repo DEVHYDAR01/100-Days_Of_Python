fe33507285d021a393e7c36442f640015845a72d
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-04-24 12:28:49 +0100

    This commit will push area cals

---
+++ day-8-8-exercise/2-Paraneters_and_Arguements.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}")
+
+# greet_with_name("billy")
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"what is it like in {location}")
+
+#Funtion with keyword argument
+greet_with(location="Abuja", name="hydar")
