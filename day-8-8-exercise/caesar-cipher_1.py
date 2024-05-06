7291e1c3232f6df3b40488b47655661109f85d32
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-05-06 20:04:04 +0100

    This commit will push decrypted function

--- day-8-8-exercise/caesar-cipher_1.py
+++ day-8-8-exercise/caesar-cipher_1.py
@@ -7,15 +7,33 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(text, shift):
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    list_of_text = []
-    for i in range(len(text)):
-        get_text = text[i]
-        if get_text in alphabet:
-            get_index = alphabet.index(get_text)
-            put_to_list = list_of_text.append(get_index)
+# def encrypt(text, shift):
+#     #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+#     list_of_text = []
+#     for i in range(len(text)):
+#         get_text = text[i]
+#         if get_text in alphabet:
+#             get_index = alphabet.index(get_text)
+#             put_to_list = list_of_text.append(get_index)
 
+#     list_of_alpha = []
+#     for i in range(shift, len(alphabet)):
+#         get_alpha = alphabet[i]
+#         get_letter = list_of_alpha.append(get_alpha)
+
+#     for i in range(0, shift):
+#         get_remainder = alphabet[i]
+#         append_to_end = list_of_alpha.append(get_remainder)
+
+#     encrypted = []
+#     for i in list_of_text:
+#         if i < len(list_of_alpha):
+#             get_the_encrypt = list_of_alpha[i]
+#             append_encrypt = encrypted.append(get_the_encrypt)
+#     encrypted_to_string = "".join(encrypted)
+#     print(f"The encoded text is {encrypted_to_string}")
+    
+def decrypt(text, shift):
     list_of_alpha = []
     for i in range(shift, len(alphabet)):
         get_alpha = alphabet[i]
@@ -25,13 +43,22 @@ def encrypt(text, shift):
         get_remainder = alphabet[i]
         append_to_end = list_of_alpha.append(get_remainder)
 
-    encrypted = []
-    for i in list_of_text:
-        if i < len(list_of_alpha):
-            get_the_encrypt = list_of_alpha[i]
-            append_encrypt = encrypted.append(get_the_encrypt)
-    encrypted_to_string = "".join(encrypted)
-    print(f"The encoded text is {encrypted_to_string}")
+    list_of_text = []
+    for i in range(len(text)):
+        get_text = text[i]
+        if get_text in list_of_alpha:
+            get_index = list_of_alpha.index(get_text)
+            put_to_list = list_of_text.append(get_index)
 
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
-encrypt(text, shift)
+    decrypted = []
+    for i in list_of_text:
+        if i < len(alphabet):
+            get_the_encrypt = alphabet[i]
+            append_encrypt = decrypted.append(get_the_encrypt)
+    decrypted_to_string = "".join(decrypted)
+    print(f"The decoded text is {decrypted_to_string}")
+
+
+#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
+# encrypt(text, shift)
+decrypt(text, shift)
