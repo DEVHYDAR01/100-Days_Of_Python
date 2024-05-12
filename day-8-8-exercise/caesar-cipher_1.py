1fa2bb4a5ca6a6bd8573055c44a7ed337a2b5acb
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-05-12 13:52:27 +0100

    This commit will push optimized code to into a single function

--- day-8-8-exercise/caesar-cipher_1.py
+++ day-8-8-exercise/caesar-cipher_1.py
@@ -6,7 +6,7 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(text, shift):
+def caesar(text, shift, direction):
     list_of_text = []
     for i in range(len(text)):
         get_text = text[i]
@@ -18,48 +18,29 @@ def encrypt(text, shift):
     for i in range(shift, len(alphabet)):
         get_alpha = alphabet[i]
         get_letter = list_of_alpha.append(get_alpha)
-
+    
     for i in range(0, shift):
         get_remainder = alphabet[i]
         append_to_end = list_of_alpha.append(get_remainder)
-
-    encrypted = []
-    for i in list_of_text:
-        if i < len(list_of_alpha):
-            get_the_encrypt = list_of_alpha[i]
-            append_encrypt = encrypted.append(get_the_encrypt)
-    encrypted_to_string = "".join(encrypted)
-    print(f"The encoded text is {encrypted_to_string}")
-
-def decrypt(text, shift):
-    list_of_alpha = []
-    for i in range(shift, len(alphabet)):
-        get_alpha = alphabet[i]
-        get_letter = list_of_alpha.append(get_alpha)
-
-    for i in range(0, shift):
-        get_remainder = alphabet[i]
-        append_to_end = list_of_alpha.append(get_remainder)
-
-    list_of_text = []
-    for i in range(len(text)):
-        get_text = text[i]
-        if get_text in list_of_alpha:
-            get_index = list_of_alpha.index(get_text)
-            put_to_list = list_of_text.append(get_index)
-
-    decrypted = []
-    for i in list_of_text:
-        if i < len(alphabet):
-            get_the_encrypt = alphabet[i]
-            append_encrypt = decrypted.append(get_the_encrypt)
-    decrypted_to_string = "".join(decrypted)
-    print(f"The decoded text is {decrypted_to_string}")
+    
+    if direction == 'encode':
+        encrypted = []
+        for i in list_of_text:
+            if i < len(list_of_alpha):
+                get_the_encrypt = list_of_alpha[i]
+                append_encrypt = encrypted.append(get_the_encrypt)
+                encrypted_to_string = "".join(encrypted)
+        print(f"The {direction}d text is {encrypted_to_string}")
+    elif direction == 'decode':
+        decrypted = []
+        for i in list_of_text:
+            if i < len(alphabet):
+                get_the_encrypt = alphabet[i]
+                append_encrypt = decrypted.append(get_the_encrypt)
+                decrypted_to_string = "".join(decrypted)
+        print(f"The {direction}d text is {decrypted_to_string}")
+    else:
+        print("enter what you wnat to do")
 
 #TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
-if direction == 'encode':
-    encrypt(text, shift)
-elif direction == 'decode':
-    decrypt(text, shift)
-else:
-    print("enter what you want me to do: ")
+caesar(text, shift, direction)
