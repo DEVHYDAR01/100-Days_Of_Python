d920be61c37ff70f15b4049eb4c60faf0e66f306
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-05-12 17:41:02 +0100

    This commit will push the cipher as a single function

--- day-8-8-exercise/caesar-cipher_1.py
+++ day-8-8-exercise/caesar-cipher_1.py
@@ -1,29 +1,33 @@
 #!/usr/bin/env python3
-
+from art import logo
+print(logo)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def caesar(text, shift, direction):
-    list_of_text = []
-    for i in range(len(text)):
-        get_text = text[i]
-        if get_text in alphabet:
-            get_index = alphabet.index(get_text)
-            put_to_list = list_of_text.append(get_index)
+if shift > len(alphabet):
+    shift = shift % 25
 
-    list_of_alpha = []
-    for i in range(shift, len(alphabet)):
-        get_alpha = alphabet[i]
-        get_letter = list_of_alpha.append(get_alpha)
-    
-    for i in range(0, shift):
-        get_remainder = alphabet[i]
-        append_to_end = list_of_alpha.append(get_remainder)
-    
+def caesar(text, shift, direction):
     if direction == 'encode':
+        list_of_text = []
+        for i in range(len(text)):
+            get_text = text[i]
+            if get_text in alphabet:
+                get_index = alphabet.index(get_text)
+                put_to_list = list_of_text.append(get_index)
+
+        list_of_alpha = []
+        for i in range(shift, len(alphabet)):
+            get_alpha = alphabet[i]
+            get_letter = list_of_alpha.append(get_alpha)
+            
+        for i in range(0, shift):
+            get_remainder = alphabet[i]
+            append_to_end = list_of_alpha.append(get_remainder)
+
         encrypted = []
         for i in list_of_text:
             if i < len(list_of_alpha):
@@ -32,6 +36,21 @@ def caesar(text, shift, direction):
                 encrypted_to_string = "".join(encrypted)
         print(f"The {direction}d text is {encrypted_to_string}")
     elif direction == 'decode':
+        list_of_alpha = []
+        for i in range(shift, len(alphabet)):
+            get_alpha = alphabet[i]
+            get_letter = list_of_alpha.append(get_alpha)
+            
+        list_of_text = []
+        for i in range(len(text)):
+            get_text = text[i]
+            if get_text in alphabet:
+                get_index = list_of_alpha.index(get_text)
+                put_to_list = list_of_text.append(get_index)
+        
+        for i in range(0, shift):
+            get_remainder = alphabet[i]
+            append_to_end = list_of_alpha.append(get_remainder)
         decrypted = []
         for i in list_of_text:
             if i < len(alphabet):
@@ -39,7 +58,5 @@ def caesar(text, shift, direction):
                 append_encrypt = decrypted.append(get_the_encrypt)
                 decrypted_to_string = "".join(decrypted)
         print(f"The {direction}d text is {decrypted_to_string}")
-    else:
-        print("enter what you wnat to do")
 
 caesar(text, shift, direction)
