83cee9804e44fc8d9815d73e59461178377f21b3
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-05-25 22:32:50 +0100

    This commit will push task 3

--- day-9-9-exercise/3-dict_in_list.py
+++ day-9-9-exercise/3-dict_in_list.py
@@ -15,10 +15,8 @@ travel_log = [
 
 #TODO: Write the function that will allow new countries
 #to be added to the travel_log. 👇
-
-
-
-
+def add_new_country(country, visits, cities):
+    travel_log.append({"country": country, "visits": visits, "cities": cities})
 
 #🚨 Do not change the code below
 add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
