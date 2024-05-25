841c787b11bfa44436612eb2a79e4cf9670fa261
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-05-25 19:51:10 +0100

    This commit will push task 2

---
+++ day-9-9-exercise/2-Nesting.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+travel_log = {
+    "France": {"cities_visited": ["Paris", "Lille", "Dijon"]},
+    "Germany": ["Berlin", "Hamburg", "Stuttgart"]
+}
+print(travel_log["France"]["cities_visited"][1])
