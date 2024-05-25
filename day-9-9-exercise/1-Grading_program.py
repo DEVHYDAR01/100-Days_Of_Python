9f0dbae419a512e4e7929492518157f9352a4efe
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-05-25 11:17:31 +0100

    This commit will push task 1

---
+++ day-9-9-exercise/1-Grading_program.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+# 🚨 Don't change the code above 👆
+
+#TODO-1: Create an empty dictionary called student_grades.
+student_grades = {}
+
+#TODO-2: Write your code below to add the grades to student_grades.👇
+
+for key in student_scores:
+    names = key
+    scores = student_scores[key]
+    # print(names)
+    # print(scores)
+    if scores == 91 or scores > 91:
+        student_grades[names] = "Oustanding"
+    elif scores == 81 or scores > 81:
+        student_grades[names] = "Exceeds Expectations"
+    elif scores == 71 or scores > 71:
+        student_grades[names] = "Acceptable"
+    elif scores == 70 or scores < 70:
+        student_grades[names] = "Fail"
+
+# 🚨 Don't change the code below 👇
+print(student_grades)
