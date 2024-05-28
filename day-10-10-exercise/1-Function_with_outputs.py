f674ef946b35142a8b42f4a884f143322d84e7bd
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-05-28 13:36:30 +0100

    This commit will push task 1

---
+++ day-10-10-exercise/1-Function_with_outputs.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def format_name(f_name, l_name):
+    name1 = f_name.title()
+    name2 = l_name.title()
+
+    return f"{name1} {name2}"
+    
+print(format_name("HYdaR", "nANa"))
