c1fee3ab5c1bd5df3b78ceffed711ce3c1a1dc3d
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-06-05 20:26:18 +0100

    This commit will push from days_in_months

---
+++ day-10-10-exercise/2-day_in_months.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+def is_leap(year):
+  if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        return True
+      else:
+        return False
+    else:
+      return True
+  else:
+    return False
+  
+
+# # # TODO: Add more code here 👇
+def days_in_month(year, month):
+  is_leap_checking = is_leap(year)
+  print(is_leap_checking)
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  if is_leap_checking == False:
+    for days in range(len(month_days)):
+      if days == month - 1:
+        return month_days[days]
+  if is_leap_checking == True and month == 2:
+    return 29
+  
+# #🚨 Do NOT change any of the code below 
+year = int(input()) # Enter a year
+month = int(input()) # Enter a month
+days = days_in_month(year, month)
+print(days)
