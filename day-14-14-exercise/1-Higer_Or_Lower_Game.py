3d4ae54cc3e719c798b8e9fb8d2bf2c0df1a8c2f
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-06-23 13:11:51 +0100

    This commit will push updates

--- day-14-14-exercise/1-Higer_Or_Lower_Game.py
+++ day-14-14-exercise/1-Higer_Or_Lower_Game.py
@@ -23,6 +23,7 @@ compare_b = {}
 update_a = dict_update(compare_a)
 update_b = dict_update(compare_b)
 
+
 def get_values(compare, which):
     """This funtion will take in a dictionary and string of what i am comparing,extract the values of the corresponding keys and returns them."""
     get_name = compare["name"]
@@ -45,5 +46,20 @@ get_follower_count_b = extract_follower_count(compare_b)
 print(get_follower_count_a)
 print(get_follower_count_b)
 
+
+# isTrue = True
+# while isTrue:
+score_count = 0
+more_followers = input("Who has more followers? Type 'A' or 'B': ").lower()
+if more_followers == 'a':
+    if get_follower_count_a > get_follower_count_b:
+        score_count = score_count + 1
+    else:
+        print(F"sorry, that's wrong. Final score: {score_count}")
+        isTrue = False
+elif more_followers == 'b':
+    if get_follower_count_b > get_follower_count_a:
+        score_count = score_count + 1
+print(score_count)
 
 print(f"The length of data if {len(data)}")
