22b6b893eb8a9056cd470a274d2d849e743176f8
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-06-28 11:43:57 +0100

    This commit will push advanced add recipe function

--- CoffeMachine/main.py
+++ CoffeMachine/main.py
@@ -29,20 +29,47 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
+cost = {"money": 0}
 
 
 def report(resource):
+    """This function will take in the resource and print the report"""
     for key in resources:
         get_key = key
         get_value = resources[key]
         print(f"{get_key}: {get_value}")
 
 
-# TODO: 1 print report of resources
+def cost_update():
+    """This function will print cost of the cost dict for now"""
+    for key in cost:
+        get_key = key
+        get_value = cost[key]
+        return f"{get_key}: {get_value}"
+
+
+def get_drink_recipe(menu):
+    get_ingredients = MENU[prefer]
+    recipe = get_ingredients["ingredients"]
+    print(recipe)
+
+
+# # TODO: 1 print report of resources
 prefer = input("What would you like? (espresso/latte/cappuccino): ").lower()
 if prefer == "report":
     report(resources)
+    update_money = cost_update()
+    print(update_money)
+elif prefer == "espresso":
+    get_drink_recipe(MENU)
 
+elif prefer == "latte":
+    get_drink_recipe(MENU)
+
+elif prefer == "cappuccino":
+    get_drink_recipe(MENU)
+
+# elif prefer == "off":
 
 
 # TODO: 2 check resource sufficient
