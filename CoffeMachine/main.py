1b43acf7e8b82a7a089b45e33edf06681276b16d
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-07-10 23:51:02 +0100

    This commit will push rask

--- CoffeMachine/main.py
+++ CoffeMachine/main.py
@@ -32,60 +32,27 @@ resources = {
 cost = {"money": 0}
 
 
-def report(resource):
-    """This function will take in the resource and print the report"""
-    for key in resources:
-        get_key = key
-        get_value = resources[key]
-        print(f"{get_key}: {get_value}")
-
-
-def cost_update():
-    """This function will print cost of the cost dict for now"""
-    for key in cost:
-        get_key = key
-        get_value = cost[key]
-        return f"{get_key}: {get_value}"
-
-
-def get_drink_recipe(menu):
-    get_ingredients = MENU[prefer]
-    recipe = get_ingredients["ingredients"]
-    return recipe
-
-
-def get_drink_recipe_keys(recipe):
-    get_water = drink_recipe["water"]
-    if "milk" in drink_recipe:
-        get_milk = drink_recipe["milk"]
-    else:
-        get_milk = 0
-    get_coffee = drink_recipe["coffee"]
-    return f"water is {get_water} milk is {get_milk} coffee is {get_coffee}"
-
-
 # # TODO: 1 print report of resources
+def report():
+    water_report = resources["water"]
+    milk_report = resources["milk"]
+    coffe_report = resources["coffee"]
+    print(f"water: {water_report}")
+    print(f"milk: {milk_report}")
+    print(f"water: {coffe_report}")
+
+
+def recipe():
+    ingredient = MENU[prefer]["ingredients"]
+    return ingredient
+
+
 prefer = input("What would you like? (espresso/latte/cappuccino): ").lower()
-if prefer == "report":
-    report(resources)
-    update_money = cost_update()
-    print(update_money)
-elif prefer == "espresso":
-    drink_recipe = get_drink_recipe(MENU)
-    print(drink_recipe)
-    recipe_keys = get_drink_recipe_keys(drink_recipe)
-    print(recipe_keys)
+ingredients = recipe()
 
-elif prefer == "latte":
-    drink_recipe = get_drink_recipe(MENU)
-    print(drink_recipe)
-    recipe_keys = get_drink_recipe_keys(drink_recipe)
-    print(recipe_keys)
 
-# elif prefer == "cappuccino":
 
-# elif prefer == "off":
-
+# report()
 
 # TODO: 2 check resource sufficient
 
