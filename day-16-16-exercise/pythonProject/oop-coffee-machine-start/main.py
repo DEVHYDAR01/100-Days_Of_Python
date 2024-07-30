b8ef81561a7af3021d097233815a73062ba9f6cf
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-07-30 13:42:45 +0100

    This commit will push oop coffee machine

--- day-16-16-exercise/pythonProject/oop-coffee-machine-start/main.py
+++ day-16-16-exercise/pythonProject/oop-coffee-machine-start/main.py
@@ -10,13 +10,23 @@ maker_of_coffee = CoffeeMaker()
 money_printer = MoneyMachine()
 menu_checker = Menu()
 
-print("is available", menu_checker.get_items())
-# is_on = True
-# while is_on:
-#     choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
-#     if choice == "off":
-#         is_on = False
-#     elif choice == "report":
-#         maker_of_coffee.report()
-#         money_printer.report()
+drinks = ["espresso", "latte", "cappuccino"]
 
+# print("is available", menu_checker.get_items())
+
+is_on = True
+while is_on:
+    choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        maker_of_coffee.report()
+        money_printer.report()
+    else:
+        for drink in drinks:
+            if choice == drink:
+                found_drink = menu_checker.find_drink(drink)
+                if maker_of_coffee.is_resource_sufficient(found_drink):
+                    cost_drink = found_drink.cost
+                    money_printer.make_payment(cost_drink)
+                    maker_of_coffee.make_coffee(found_drink)
