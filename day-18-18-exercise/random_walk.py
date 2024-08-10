1436c47bd44c5288d7f36783a3c1235b34d06887
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-08-10 10:40:42 +0100

    This commit will push task as both approaches

--- day-18-18-exercise/random_walk.py
+++ day-18-18-exercise/random_walk.py
@@ -5,45 +5,53 @@ nana = Turtle()
 colors = ["dark green", "dark slate gray", "maroon", "green yellow", "indigo", "medium violet red"]
 
 
-def forward(steps):
+# def forward(steps):
+#     nana.color(random.choice(colors))
+#     nana.pensize(5)
+#     nana.speed("fastest")
+#     return nana.forward(steps)
+#
+#
+# def backward(steps):
+#     nana.color(random.choice(colors))
+#     nana.pensize(5)
+#     nana.speed("fastest")
+#     return nana.backward(steps)
+#
+#
+# def right(steps):
+#     nana.color(random.choice(colors))
+#     nana.pensize(5)
+#     nana.speed("fastest")
+#     return nana.right(steps)
+#
+#
+# def left(steps):
+#     nana.color(random.choice(colors))
+#     nana.pensize(5)
+#     nana.speed("fastest")
+#     return nana.left(steps)
+
+motion = [0, 90, 180, 270, 360]
+
+for i in range(1000):
     nana.color(random.choice(colors))
     nana.pensize(5)
     nana.speed("fastest")
-    return nana.forward(steps)
+    nana.forward(10)
+    nana.right(random.choice(motion))
 
+# motion = [forward, backward, right, left]
 
-def backward(steps):
-    nana.color(random.choice(colors))
-    nana.pensize(5)
-    nana.speed("fastest")
-    return nana.backward(steps)
-
-
-def right(steps):
-    nana.color(random.choice(colors))
-    nana.pensize(5)
-    nana.speed("fastest")
-    return nana.right(steps)
-
-
-def left(steps):
-    nana.color(random.choice(colors))
-    nana.pensize(5)
-    nana.speed("fastest")
-    return nana.left(steps)
-
-
-motion = [forward, backward, right, left]
-
-for i in range(2000):
-    random_walk = random.choice(motion)
-    print(random_walk)
-    if random_walk == forward:
-        random_walk(10)
-    elif random_walk == backward:
-        random_walk(10)
-    else:
-        random_walk(90)
+# for i in range(2000):
+#     random_walk = random.choice(motion)
+#     print(random_walk)
+#     if random_walk == forward:
+#         random_walk(10)
+#     elif random_walk == backward:
+#         random_walk(10)
+#     else:
+#         random_walk(90)
 
 
 my_screen = Screen()
