73d6f90899bd73ac54d82aa04905e0d5a5fb0b3e
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-09-10 19:43:06 +0100

    This commit will push day etching game implemented concept of higher order funtion assimulated

--- day-19-19-exercise/main.py
+++ day-19-19-exercise/main.py
@@ -2,13 +2,38 @@ from turtle import Turtle, Screen
 
 tess = Turtle()
 screen = Screen()
-screen.listen()
 
 
 def forward():
     tess.fd(10)
 
 
-screen.onkey(key="w", fun=forward)
+def backward():
+    tess.bk(10)
 
+
+def clockwise():
+    tess.right(10)
+
+
+def counter_clockwise():
+    tess.left(10)
+
+
+def right_forward():
+    tess.right(10)
+    tess.forward(10)
+
+
+def clear_origin():
+    screen.resetscreen()
+
+
+screen.onkey(key="s", fun=backward)
+screen.onkey(key="w", fun=forward)
+screen.onkey(key="d", fun=clockwise)
+screen.onkey(key="a", fun=counter_clockwise)
+screen.onkey(key="c", fun=clear_origin)
+
+screen.listen()
 screen.exitonclick()
