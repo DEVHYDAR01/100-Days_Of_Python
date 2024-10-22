cc24f1a4155ba9a0f30ec6a6c79da7b4d97441e0
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-10-22 11:35:08 +0100

    This commit will push working with pandas

--- day-25-25-exercise/main.py
+++ day-25-25-exercise/main.py
@@ -1,3 +1,14 @@
-with open('./weather_data .csv') as weather_data:
-    content = weather_data.readlines()
-    print(content)
+# with open('./weather_data.csv') as weather_data:
+#     data = weather_data.readlines()
+#     print(data)
+
+import csv
+
+with open("./weather_data.csv") as weather_data:
+    data = csv.reader(weather_data)
+    temperature = []
+    for row in data:
+        if row[1] != "temp":
+            temperature.append(int(row[1]))
+
+    print(temperature)
