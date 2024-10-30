a43bbc7336aaff553fbfd4b19b5e2081c14d7342
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-10-30 12:24:49 +0100

    This commit will push learning about widget and componenets of the tkinter lib

--- day-27-27-exercise/main.py
+++ day-27-27-exercise/main.py
@@ -1,18 +1,18 @@
 from tkinter import *
 
+
+def button_clicked():
+    get_val = enter.get()
+    my_label["text"] = get_val
+
+
 windows = Tk()
 windows.minsize(width=500, height=300)
 windows.title("My First GUI")
 
 my_label = Label(text="I am label", font=("arial", 8, "bold"))
-my_label.pack()
-
 my_label["text"] = "New Text"
-
-
-def button_clicked():
-    get_val = enter.get()
-    my_label["text"] = get_val
+my_label.pack()
 
 
 # button
