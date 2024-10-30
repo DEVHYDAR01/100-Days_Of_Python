800980b129f81889dda1d05d4c571d4d7e603652
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-10-30 12:08:29 +0100

    This commit will push learning about widget and componenets of the tkinter lib

--- day-27-27-exercise/main.py
+++ day-27-27-exercise/main.py
@@ -1,11 +1,27 @@
-import tkinter
+from tkinter import *
 
-windows = tkinter.Tk()
+windows = Tk()
 windows.minsize(width=500, height=300)
 windows.title("My First GUI")
 
-my_label = tkinter.Label(text="I am label", font=("arial", 8, "bold"))
+my_label = Label(text="I am label", font=("arial", 8, "bold"))
 my_label.pack()
+
+my_label["text"] = "New Text"
+
+
+def button_clicked():
+    get_val = enter.get()
+    my_label["text"] = get_val
+
+
+# button
+button = Button(text="Click me", command=button_clicked)
+button.pack()
+
+enter = Entry(width=10)
+enter.pack()
+
 
 
 
--- day-27-27-exercise/playground.py
+++ day-27-27-exercise/playground.py
@@ -1,8 +1,18 @@
-def add(*args):
-    total = 0
-    for n in args:
-        total += n
-    return total
+# def add(*args):
+#     total = 0
+#     for n in args:
+#         total += n
+#     return total
+#
+#
+# print(add(2, 3, 4, 110))
 
+# def calculate(n, **kwargs):
+#     print(kwargs)
+#     n += kwargs["add"]
+#     n *= kwargs["multiply"]
+#     print(n)
+#
+#
+# calculate(2, add=3, multiply=5)
 
-print(add(2, 3, 4, 110))
