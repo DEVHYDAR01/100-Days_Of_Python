573c2d0de9df518838d1bf21c0034e7f66febdb2
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-11-03 20:54:05 +0100

    This commit will push the implemetation of the promodoro technique timer with almost full functionality

--- day-28-28-exercise/main.py
+++ day-28-28-exercise/main.py
@@ -1,28 +1,78 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
+WORK_MIN = 1
+SHORT_BREAK_MIN = 1
 LONG_BREAK_MIN = 20
-# ---------------------------- TIMER RESET ------------------------------- # 
+reps = 0
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_sec = SHORT_BREAK_MIN * 60
+    long_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        timer_label.config(text="Break", fg=RED)
+        count_down(long_sec)
+    elif reps % 2 == 0:
+        timer_label.config(text="Break", fg=PINK)
+        count_down(short_sec)
+    else:
+        timer_label.config(text="Work", fg=GREEN)
+        count_down(work_sec)
+    # print(reps)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    count_mins = math.floor(count / 60)
+    count_secs = count % 60
+    if count_secs < 10:
+        count_secs = f"0{count_secs}"
+    canvas.itemconfig(timer_text, text=f"{count_mins}:{count_secs}")
+    if count > 0:
+        windows.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        mark = ""
+        for _ in range(math.floor(reps/2)):
+            mark += "✔"
+            checkmark_label.config(text=mark)
 
 # ---------------------------- UI SETUP ------------------------------- #
 windows = Tk()
 windows.title("Promodoro")
 windows.config(padx=100, pady=50, bg=YELLOW)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 picture = PhotoImage(file="./tomato.png")
 canvas.create_image(100, 112, image=picture)
-canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+timer_text = canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+timer_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 40, "bold"))
+timer_label.grid(column=1, row=0)
+
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
+start_button.grid(column=0, row=2)
+
+
+checkmark_label = Label(fg=GREEN, bg=YELLOW)
+checkmark_label.grid(column=1, row=3)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
 
 
 
