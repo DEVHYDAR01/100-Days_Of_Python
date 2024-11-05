6a674e46ef57167a254e6b91ac4c8073375b00e2
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-11-05 21:59:19 +0100

    This commit will push day 29 dir

---
+++ day-29-29-exercise/main.py
@@ -0,0 +1,40 @@
+from tkinter import *
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+windows = Tk()
+windows.title("Password Manager")
+windows.config(padx=20, pady=20)
+
+canvas = Canvas(width=200, height=200)
+picture = PhotoImage(file="./logo.png")
+canvas.create_image(100, 100, image=picture)
+canvas.grid(column=1, row=0)
+
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+
+website_input = Entry(width=35)
+website_input.grid(column=1, row=1)
+
+emailuser_label = Label(text="Email/Username:")
+emailuser_label.grid(column=0, row=2)
+
+emailuser_input = Entry(width=35)
+emailuser_input.grid(column=1, row=2)
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+
+password_input = Entry(width=21)
+password_input.grid(column=1, row=3)
+
+generate_button = Button(text="Generate Password")
+generate_button.grid(column=2, row=3)
+
+add_button = Button(text="Add", width=36)
+add_button.grid(column=1, row=4)
+
+windows.mainloop()
