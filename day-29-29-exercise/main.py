1a1f2ea3089198e5a708e7a441a4fccad2c76042
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-11-06 23:49:48 +0100

    This commit will push all full funtionalities including clipboard capabilities

--- day-29-29-exercise/main.py
+++ day-29-29-exercise/main.py
@@ -1,6 +1,29 @@
 from tkinter import *
 from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+#Password Generator Project
+
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    letter_list = [choice(letters) for char in range(randint(8, 10))]
+    symbols_list = [choice(symbols) for sym in range(randint(2, 4))]
+    numbers_list = [choice(numbers) for num in range(randint(2, 4))]
+
+    password_list = letter_list + symbols_list + numbers_list
+    shuffle(password_list)
+
+    password = "".join(password_list)
+
+    password_input.insert(END, password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
@@ -39,7 +62,7 @@ emailuser_label.grid(row=2, column=0)
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
-# enteries
+# entries
 website_input = Entry(width=35)
 website_input.grid(row=1, column=1, columnspan=2)
 website_input.focus()
@@ -52,7 +75,7 @@ password_input = Entry(width=18)
 password_input.grid(row=3, column=1)
 
 # buttons
-generate_button = Button(text="Generate Password", width=13)
+generate_button = Button(text="Generate Password", width=13, command=generate_password)
 generate_button.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=32, command=save)
