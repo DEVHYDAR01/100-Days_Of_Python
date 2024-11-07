1b229a4a5b5d7b88d6ecc34a3975172f8ba69f55
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-11-07 23:05:13 +0100

    This commit will push revamped the approach to use try except and else to solve the issue and with a sprinkle of recursion

--- day-26-26-exercise/main.py
+++ day-26-26-exercise/main.py
@@ -3,14 +3,17 @@ import pandas
 df = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in df.iterrows()}
 
-is_on = True
-while is_on:
+def re_run():
+    users_input = input("Enter a word: ").upper()
+    convert_to_list = list(users_input)
     try:
-        users_input = input("Enter a word: ").upper()
-        convert_to_list = list(users_input)
         phonetic_words_list = [nato_dict[letter] for letter in convert_to_list]
-        print(phonetic_words_list)
-        is_on = False
+
     except KeyError:
         print("Sorry only letters in the alphabets")
-        
+        re_run()
+    else:
+        print(phonetic_words_list)
+
+
+re_run()
