788d640922b9e4828628ac677353d003877f2806
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-12-26 21:30:55 +0100

    This commit will push day 47 as done

--- day-47-47-exercise/main.py
+++ day-47-47-exercise/main.py
@@ -5,7 +5,8 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-testing_endpoint = "https://appbrewery.github.io/instant_pot/"
+# testing_endpoint = "https://appbrewery.github.io/instant_pot/"
+testing_endpoint = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
 browser_des = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"
 amazon_header = {
     "User-Agent": browser_des,
@@ -15,10 +16,11 @@ amazon_header = {
 response = requests.get(url=testing_endpoint, headers=amazon_header)
 response.raise_for_status()
 sample_page = response.text
-# print(sample_page)
+print(sample_page)
 
 soup = BeautifulSoup(sample_page, "html.parser")
-price = float(soup.find(name="span", class_="aok-offscreen").get_text().split("$")[1])
+print(soup.prettify())
+price = float(soup.find(name="span", class_="a-offscreen").get_text().split("$")[1])
 get_product_title = soup.find(name="span", id="productTitle").get_text()
 split_text = get_product_title.split()
 product_title = "".join(split_text).replace(",", " ")
