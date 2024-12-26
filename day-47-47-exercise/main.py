0ad38d4a02645c021e885d948a832a54869fb08d
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-12-26 02:00:39 +0100

    This commit will push day 47 with the implementation of the amazon price tracker bot

--- day-47-47-exercise/main.py
+++ day-47-47-exercise/main.py
@@ -1,7 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 import smtplib
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 testing_endpoint = "https://appbrewery.github.io/instant_pot/"
 
 response = requests.get(url=testing_endpoint)
@@ -11,6 +14,18 @@ sample_page = response.text
 
 soup = BeautifulSoup(sample_page, "html.parser")
 price = float(soup.find(name="span", class_="aok-offscreen").get_text().split("$")[1])
-print(price)
-
-
+get_product_title = soup.find(name="span", id="productTitle").get_text()
+split_text = get_product_title.split()
+product_title = "".join(split_text).replace(",", " ")
+# print(product_title)
+# print(price)
+target_price = 100.00
+if price < target_price:
+    with smtplib.SMTP(os.getenv("SMTP_ADDRESS"), port=587) as connection:
+        connection.starttls()
+        connection.login(user=os.getenv("EMAIL_ADDRESS"), password=os.getenv("EMAIL_PASSWORD"))
+        connection.sendmail(
+            from_addr=os.getenv("EMAIL_ADDRESS"),
+            to_addrs="autobirthdaywisherinfo@gmail.com",
+            msg=f"Subject: AMAZON PRICE ALERT\n\n{product_title} is now ${price}\n\n{testing_endpoint}".encode()
+        )
